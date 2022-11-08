82b95523f52d910cc5b594ffa4abf65e036ef0c3
Muhammad Ali Murtaza <muhammadalimurtaza997@gmail.com>
2022-11-08 12:04:49 +0500

    Express JS Structure project Added

--- Mern-Tutorial_JavaScriptMastery/backend/server.js
+++ Mern-Tutorial_JavaScriptMastery/backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/middleware')
 const app = express()
 
+
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
